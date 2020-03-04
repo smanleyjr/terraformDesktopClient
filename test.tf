80ceb73196afdd49b7069d3fd32254593e957d1e
smanley1 <sheltonmanley@quickenloans.com>
2020-03-04 16:36:40 -0500

    adding these testfiles

---
+++ test.tf
@@ -0,0 +1,15 @@
+provider "aws" {
+  profile = "default"
+  region  = var.region
+}
+resource "aws_instance" "example" {
+  ami           = "ami-b374d5a5"
+  instance_type = "t2.micro"
+  provisioner "local-exec" {
+    command = "echo ${aws_instance.example.public_ip} > ip_address.txt"
+  }
+}
+resource "aws_eip" "ip" {
+  vpc      = true
+  instance = aws_instance.example.id
+}
